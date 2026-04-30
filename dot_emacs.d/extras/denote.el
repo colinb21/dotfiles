0f5ff1d37a331b08eb232b7e1210faf07804c379
colinb21 <cdabenson@gmail.com>
2026-04-30 11:41:28 +0100

    add denote-journal.

--- dot_emacs.d/extras/denote.el
+++ dot_emacs.d/extras/denote.el
@@ -40,6 +40,24 @@
     denote-org-dblock-insert-missing-links
     denote-org-dblock-insert-files-as-headings))
 
+(use-package denote-journal
+  :ensure t
+  ;; Bind those to some key for your convenience.
+  :commands ( denote-journal-new-entry
+              denote-journal-new-or-existing-entry
+              denote-journal-link-or-create-entry )
+  :hook (calendar-mode . denote-journal-calendar-mode)
+  :config
+  ;; Use the "journal" subdirectory of the `denote-directory'.  Set this
+  ;; to nil to use the `denote-directory' instead.
+  (setq denote-journal-directory
+        (expand-file-name "journal" denote-directory))
+  ;; Default keyword for new journal entries. It can also be a list of
+  ;; strings.
+  (setq denote-journal-keyword "journal")
+  ;; Read the doc string of `denote-journal-title-format'.
+  (setq denote-journal-title-format 'day-date-month-year))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;           denote meeting notes
 ;;
