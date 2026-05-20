09dff72b979e4838e7b4d620a19ec56285e4dfdc
colinb21 <cdabenson@gmail.com>
2026-05-20 11:55:45 +0100

    cleanup, delete old crap, use new screen-grid-stuff.

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,4 +1,3 @@
-
 hs.alert.show("Config loaded")
 --hs.loadSpoon("WinWin")
 --hs.loadSpoon("WindowHalfsAndThirds")
@@ -9,73 +8,9 @@ function bindKey(key, fn)
   hs.hotkey.bind(cac, key, fn)
 end
 
-bindKey('l', function() hs.grid.show() end)
-
---bindKey('h', function() spoon.WinWin:stepResize('left') end)
---bindKey(';', function() spoon.WinWin:stepResize('right') end)
-
--- bindKey(';', function() spoon.WinWin:moveToScreen('left') end)
--- bindKey('\\', function() spoon.WinWin:moveToScreen('right') end)
-
--- bindKey('i', function() spoon.WinWin:moveAndResize('halfup') end)
-
--- bindKey('j', function() spoon.WinWin:moveAndResize('halfleft') end)
--- bindKey('k', function() spoon.WinWin:moveAndResize('center') end)
--- bindKey('l', function() spoon.WinWin:moveAndResize('halfright') end)
-
---bindKey('m', function() spoon.WinWin:moveAndResize('cornerSW') end)
--- bindKey(',', function() spoon.WinWin:moveAndResize('halfdown') end)
-
--- bindKey('f', function() hs.window.focusedWindow():toggleFullScreen() end)
-
--- hs.loadSpoon("KSheet")
--- bindKey('c', function() spoon.KSheet:show() end)
--- bindKey('x', function() spoon.KSheet:hide() end)
-
-
--- Set correct screen rotation
-
--- function rotate()
---    thisScreen=hs.mouse.getCurrentScreen()
---    rotation=thisScreen:rotate()
---    thisScreen:rotate((rotation+90) % 360)
--- end
-
-
--- bindKey('S', function() rotate() end)
-
----
---- Detect displays when mash-A is pressed
----
--- function detect_displays()
---    hs.applescript.applescript([[
--- tell application "System Preferences"
---     activate
---     reveal pane "com.apple.preference.displays"
--- end tell
-
--- delay 0.5
-
--- tell application "System Events"
---     tell process "System Preferences"
---         try --don't even consider not using a try block!
---             key down option
---             delay 0.2
---             click button "Detect Displays" of window 1
---             delay 0.2
---             key up option
---         on error --logging out is the only other way to clear these
---             key up option
---         end try
---     end tell
---     tell process "System Preferences"
---        quit
---     end tell
--- end tell
--- ]])
--- end
-
--- bindKey('A', function() detect_displays() end)
+ -- Keybindings cheat-sheet for current application. Press to toggle.
+hs.loadSpoon("KSheet")
+bindKey('c', function() spoon.KSheet:toggle() end)
 
 -- Rescue Windows
 -- Move any windows that are off-screen onto the main screen
@@ -96,30 +31,7 @@ end)
 
 bindKey('D', function() hs.execute("open ~/Downloads") end)
 
-function help()
-   return [[
-     mash + ; - move screen left     mash + \ - move screen right
-
-                            mash + i - move N
-     mash + j - move W      mash + k - move centre     mash + l - move E
-                            mash + , - move S
-
-     mash + f - fullscreen
-
-     mash + c - cheatsheet     mash + x - exit cheatsheet
-
-     mash + s - rotate 90 degrees     mash + a - detect displays     mash + r - rescue lost windows
-
-     mash + h - help. This screen     mash + d - Open Downloads
-]]
-end
-
-local helpStyle = {}
-helpStyle['textFont'] = 'Courier New'
-helpStyle['radius'] = 10
-helpStyle['textSize'] = 14
-
-
+-- stay awake
 hs.loadSpoon('Caffeine')
 spoon.Caffeine:start()
 
@@ -144,3 +56,4 @@ end
 -- hs.loadSpoon('Cherry')
 
 require "fetch-page-helper"
+require "screen-grid-stuff"
