cd9da34d8552204f6491c912f00fcd5914c567ce
colinb21 <cdabenson@gmail.com>
2026-06-02 19:56:36 +0100

    improved sdcv

--- dot_emacs.d/extras/spelling.el
+++ dot_emacs.d/extras/spelling.el
@@ -12,17 +12,37 @@
 (setopt dictionary-use-single-buffer t)
 (setopt dictionary-server "dict.org")
 
-;; Interface for sdcv (StartDict console version).
+;; Interface for sdcv (StartDict console version)..
+;;
+;; largely stolen from
+;; https://github.com/jamescherti/quick-sdcv.el/discussions/6
 (use-package quick-sdcv
-  :ensure t
-  :straight (quick-sdcv
-             :type git
-             :host github
-             :repo "jamescherti/quick-sdcv.el")
+  :bind
+  ("C-c s" . my-quick-sdcv-dwim)
+  (:map quick-sdcv-mode-map
+		("q" . quit-window)
+		("<tab>" . outline-toggle-children))
+  :hook
+  (quick-sdcv-mode . goto-address-mode)
+  :config
+  (add-to-list 'display-buffer-alist
+			   '("\\*sdcv"
+				 (display-buffer-reuse-window display-buffer-at-bottom)
+				 (window-height . 0.3)))
   :custom
-  (quick-sdcv-dictionary-prefix-symbol "►")
-  (quick-sdcv-ellipsis " ▼ "))
-(bind-key "C-c s" 'quick-sdcv-search-at-point)
+  (quick-sdcv-dictionary-prefix-symbol "▶")
+  (quick-sdcv-ellipsis " ▼")
+  (quick-sdcv-unique-buffers t))
+
+
+(defun my-quick-sdcv-dwim (&optional arg)
+  "Look up a word using quick-sdcv.
+By default, looks up the word at point.
+With a prefix argument (\\[universal-argument]), prompts for a word."
+  (interactive "P")
+  (if arg
+	  (call-interactively #'quick-sdcv-search-input)
+	(quick-sdcv-search-at-point)))
 
 ;; Cape-jinx package and configuration:
 ;; sounds wonderful but keep crashing with some sort of cache error in
