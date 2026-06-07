df6bf00ff1388c8da93c95ee8084ac199ab5fffa
colinb21 <cdabenson@gmail.com>
2026-06-07 14:12:37 +0100

    zsh cleanup and add vterm setup helpers

---
+++ dot_aliases.zsh
@@ -0,0 +1,9 @@
+alias fzfp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+alias myvm="et -c "tmux -CC new -A colin-vm" colin@colin-vm.dev.us-west-1.nexthop.ai"
+
+# first:
+# uv python install 3.14
+# Installed Python 3.14.0a4 in 1.87s
+# then
+alias python3.14='uv run --python=3.14 python3'
+
